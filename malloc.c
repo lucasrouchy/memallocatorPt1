75743a05661eb69c8c0fc2ec3f3dc1de20111377
Lucas <55973521+lucasrouchy@users.noreply.github.com>
2022-02-12 23:37:33 -0800

    first commit

---
+++ malloc.c
@@ -0,0 +1,22 @@
+void print_data(void)
+{
+    struct block *b = head;
+
+    if (b == NULL) {
+        printf("[empty]\n");
+        return;
+    }
+
+    while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
+        if (b->next != NULL) {
+            printf(" -> ");
+        }
+
+        b = b->next;
+    }
+
+    printf("\n");
+}
